fa9691dd73f452aa9a959de65ca99576f64a95c5
Cedar Pfeiffer <elipfeiffer45@gmail.com>
2025-05-11 11:51:02 +0100

    changed comments, tried out different parameters

--- testML/testParkinsons.py
+++ testML/testParkinsons.py
@@ -16,27 +16,30 @@ X, y = data[:-1, 1:], data[:-1, 0]
 #print(X)
 #print(y)
 
-#define model as xgboost regressor NO IDEA WHAT THE EVAL METRIC MEANS
+#define model as xgboost regressor (RF is random forest, without RF is linear)
 #model = XGBRFRegressor(eval_metric='rmsle')
-model = XGBRFRegressor(learning_rate= 0.4, max_depth= 4, n_estimators= 200)
 #model = XGBRegressor(eval_metric='rmsle')
 
+#setting the tree method to exact causes errors when fitting, I am not sure why
+model = XGBRFRegressor(learning_rate= 0.1, max_depth= 3, n_estimators= 30, tree_method="approx")
 
-#find the best hyper parameters
 
 """
+#find the best hyper parameters
+
 #set up our search grid
 param_grid = {"max_depth":    [2, 3, 4],
               "n_estimators": [100, 150, 200, 250, 300],
               "learning_rate": [0.35, 0.4, 0.45, 0.5]}
 
 # try out every combination of the above values
-search = GridSearchCV(model, param_grid, cv=5).fit(X, y)
+search = GridSearchCV(model, param_grid, refit=True, cv=5).fit(X, y)
 
 print("The best hyperparameters are ",search.best_params_)
+
 """
 """
-#evaluate the model NEED TO UNDERSTAND THIS BIT
+#evaluate the model
 
 #define model evaluation method
 cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=1)
@@ -48,7 +51,7 @@ print('Mean MAE: %.3f (%.3f)' % (scores.mean(), scores.std()) )
 """
 
 #fit the model to the data
-model.fit(X, y)
+model.fit(X,y)
 
 #test it, this row should result in 0 as the answer
 data = [214.289,260.277,77.973,0.00567,0.00003,0.00295,0.00317,0.00885,0.01884,0.19,0.01026,0.01161,0.01373,0.03078,0.04398,21.209,0.462803,0.664357,-5.724056,0.190667,2.555477,0.148569]
