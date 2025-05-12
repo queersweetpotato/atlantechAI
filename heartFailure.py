bf26026757f3db176191769d219b829303338bb2
Cedar Pfeiffer <elipfeiffer45@gmail.com>
2025-05-12 13:22:41 +0100

    split data, tests with default model parameters

--- heartFailure.py
+++ heartFailure.py
@@ -12,15 +12,12 @@ from sklearn.model_selection import train_test_split
 data = fetch_openml(data_id=45949)
 
 #the dataset does not have a defined target column, hence I have decided to use death event
-print("data: ", data['data'])
 y = data['data']['DEATH_EVENT']
 #need to delete final column from the data bit
+del data['data']['DEATH_EVENT']
+X = data['data']
+X_train, X_test, y_train, y_test = train_test_split(X, y)
 
-#X_train, y_test = train_test_split(data['data'], data['target'])
-#print(X_train)
-#print(X_test)
-#print(y_train)
-#print(y_test)
 
 #define model
 #model = XGBRFRegressor(eval_metric='rmsle')
@@ -28,9 +25,9 @@ model = XGBRFRegressor()
 #model = XGBRFRegressor(learning_rate= 0.1, max_depth= 3, n_estimators= 30, tree_method="approx")
 
 #train the model
-#model.fit(X, y)
+model.fit(X_train, y_train)
 
 #predict
-#preds = bst.predict(X_test)
-#print(preds)
-
+preds = model.predict(X_test)
+print(preds)
+print(y_test)
