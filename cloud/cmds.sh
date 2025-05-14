e1f22ccac1585c9ec668d9347b6ae5e728e827fa
Linux-Tech-Tips <disposablemail404.2@gmail.com>
2025-05-14 20:34:53 +0100

    Added heart failure cloud container data

--- cloud/cmds.sh
+++ cloud/cmds.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker image build --file Dockerfile --tag test:latest .
-docker tag test:latest us-central1-docker.pkg.dev/forward-alchemy-459719-j4/testrepo/test:latest
-docker push us-central1-docker.pkg.dev/forward-alchemy-459719-j4/testrepo/test:latest
+docker image build --file Dockerfile --tag predict:latest .
+docker tag predict:latest us-central1-docker.pkg.dev/forward-alchemy-459719-j4/testrepo/predict:latest
+docker push us-central1-docker.pkg.dev/forward-alchemy-459719-j4/testrepo/predict:latest
--- cloud/predictUserInput.py
+++ cloud/predictUserInput.py
@@ -7,14 +7,19 @@ def predictUserHealth(data):
   heartData = asarray([data])
  
   #load models
-  heartModel = XGBRFRegressor()
-  heartModel.load_model("../heartEvent.ubj");
-  deathModel = XGBRFRegressor()
-  deathModel.load_model("../death.ubj");
+  try:
+    heartModel = XGBRFRegressor()
+    heartModel.load_model("./heartEvent.ubj")
+    heartModel.set_params(tree_method="hist", predictor="cpu_predictor")
+    deathModel = XGBRFRegressor()
+    deathModel.load_model("./death.ubj")
+    deathModel.set_params(tree_method="hist", predictor="cpu_predictor")
+  except Exception as e:
+    print(f"Error: {e}")
  
   #first value is probability of heart problems, second is the probability of death assuming heart failure
   returnValues = [heartModel.predict(heartData), deathModel.predict(deathData)]
   
   return returnValues
 
-print(predictUserHealth([80,None,None,1,None,None,None,None,None,0,0,None,0,0,0.5,0,0,None,None,None,0,None,None,None,None,None,None,None,None,None,None,None,None,1,0,8,0,0,0,0,0,0]))
+#print(predictUserHealth([80,None,None,1,None,None,None,None,None,0,0,None,0,0,0.5,0,0,None,None,None,0,None,None,None,None,None,None,None,None,None,None,None,None,1,0,8,0,0,0,0,0,0]))
