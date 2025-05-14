0860531d74007adaa1df800de5b99885720dc614
Linux-Tech-Tips <disposablemail404.2@gmail.com>
2025-05-14 21:28:01 +0100

    Added CORS auth to cloud function and fetch call to frontend script

--- frontend/script.js
+++ frontend/script.js
@@ -5,16 +5,15 @@ const questions = [
     type: 'text',
     label: 'Number 1:',
   },
-/*  {
-    id: '2',
-    type: 'multiple',
-    label: 'How many hours of sleep do you get a night, on average?',
-    options: ['less than 4', '4-6', '6-8', 'More than 8']
-  },*/
   {
     id: '2',
     type: 'text',
     label: 'Number 2:',
+  },
+  {
+    id: '3',
+    type: 'text',
+    label: 'Number 3:',
   }
 ];
 
@@ -67,8 +66,8 @@ function generateForm(questions) {
 function handleSubmit(data) {
   // Getting parameters by number and appending them to array
   let param = {"p": data["1"]};
-  for(let i = 2; i <= questions.length; ++i) {
-    param["p"] += "," + data[i.toString()];
+  for(let i = 2; i <= 42; ++i) {
+    param["p"] += "," + data[(i % questions.length + 1).toString()];
   }
 
   // Creating query parameters
@@ -76,21 +75,17 @@ function handleSubmit(data) {
 
   // Getting data from backend
   const site = "https://predict-425025011942.us-central1.run.app/";
-  /*fetch(site + "?" + queryParams).then(response => {
+  fetch(site + "?" + queryParams).then(response => {
     if(!response.ok)
 	throw new Error("Backend Response Error");
     return response.text();
   }).then(resultText => {
     console.log("Response from backend: ", resultText);
-  }).then(error => {
+  }).catch(error => {
     console.log("Error trying to fetch from backend: ", error);
-  });*/
-  // TODO Implement fetching from server and displaying results
+  });
 
   console.log(param);
-
-  //console.log('Form submitted with data:', data);
-  // You can replace this with your scoring logic, API calls, etc.
 }
 
 // Initialize form
